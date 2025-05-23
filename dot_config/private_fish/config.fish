e694f9613f460ac9420d6c1924c7b6b154bb1695
Andreas Berger <work@bergerweb.ch>
2025-05-23 17:12:14 +0200

    adds fish config

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,20 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+abbr -a -- a 'php artisan'
+abbr -a -- arl 'php artisan route:list'
+abbr -a -- ao 'php artisan optimize'
+abbr -a -- aoc 'php artisan optimize:clear'
+abbr -a -- arc 'php artisan route:clear'
+abbr -a -- acc 'php artisan cache:clear'
+abbr -a -- am 'php artisan migrate'
+abbr -a -- amf 'php artisan migrate:fresh'
+
+abbr -a -- qd 'quasar dev'
+abbr -a -- qb 'quasar build'
+abbr -a -- qios 'npm run start:ios'
+abbr -a -- qad 'npm run start:android'
+
+abbr -a -- sshv 'ssh vipany@vipany.ch'
+abbr -a -- ssht 'ssh topofferten@top-offerten.ch'
+
